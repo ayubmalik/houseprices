b8f4a0bb0ec8b3c2e5a9f495ade61d3f2dc36ce1
Ayub Malik <ayub.malik@dwp.gsi.gov.uk>
2015-10-27 14:50:35 +0000

    Fix test

--- houseprices-core/src/main/scala/houseprices/PricePaidToJson.scala
+++ houseprices-core/src/main/scala/houseprices/PricePaidToJson.scala
@@ -1,9 +1,13 @@
 package houseprices
 
 import org.json4s.JsonDSL._
+import org.json4s._
+import org.json4s.native.Serialization
+import org.json4s.native.Serialization.{ read, write }
 
 object PricePaidToJson {
+  implicit val formats = Serialization.formats(NoTypeHints)
 
-  def apply(pricePaid: PricePaid) = ""
+  def apply(pricePaid: PricePaid) = write(pricePaid)
 }
 
--- houseprices-core/src/test/scala/houseprices/PricePaidToJsonSpec.scala
+++ houseprices-core/src/test/scala/houseprices/PricePaidToJsonSpec.scala
@@ -6,10 +6,8 @@ import org.scalatest.Matchers
 class PricePaidToJsonSpec extends FlatSpec with Matchers {
 
   "HousePriceToJson" should "convert to json" in {
-    val housePrice = PricePaid("123", 55000, "2004", Address("primaryx", "secondaryx", "streetx", "localityx", "townx", "districtx", "countyx"))
+    val housePrice = PricePaid("123", 5000, "2004", Address("p", "s", "st", "l", "t", "d", "c"))
     PricePaidToJson(housePrice) should be(
-      """{
-          "id" : "123"
-        }""")
+      """{"id":"123","price":5000,"date":"2004","address":{"primaryNameNumber":"p","secondaryNameNumber":"s","street":"st","locality":"l","townCity":"t","district":"d","county":"c"}}""")
   }
 }

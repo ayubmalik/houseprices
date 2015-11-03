c9c589c38553e6e8a726fb3ad8903d6f7c1f1ef2
Ayub Malik <ayub.malik@dwp.gsi.gov.uk>
2015-11-03 14:23:41 +0000

    Interim commit before fu

--- houseprices-rest-api/src/main/scala/example/FlowExample.scala
+++ houseprices-rest-api/src/main/scala/example/FlowExample.scala
@@ -15,7 +15,7 @@ object FlowExample extends App {
   implicit val sys = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
-  val source = Source(1 to 100)
+  val source = Source(1 to 10)
   val sink = Sink.fold[Int, Int](0)(_ + _)
   val graph: RunnableGraph[Future[Int]] = source.toMat(sink)(Keep.right)
 
--- houseprices-rest-api/src/main/scala/houseprices/restapi/HousePricesRestApi.scala
+++ houseprices-rest-api/src/main/scala/houseprices/restapi/HousePricesRestApi.scala
@@ -2,17 +2,44 @@ package houseprices.restapi
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshalling.ToResponseMarshallable.apply
+import akka.http.scaladsl.marshalling.ToResponseMarshallable._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.RouteResult.route2HandlerFlow
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.model.HttpRequest
+import akka.stream.scaladsl.{ Flow, Source, Sink }
 import akka.stream.ActorMaterializer
-import scala.collection.Searching.Found
+import scala.concurrent.Future
+import java.io.IOException
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.http.scaladsl.model.StatusCodes
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object HousePricesRestApi extends App {
 
   implicit val system = ActorSystem("houseprices-system")
   implicit val materializer = ActorMaterializer()
+
+  lazy val housePriceFlow: Flow[HttpRequest, HttpResponse, Any] = Http().outgoingConnection("localhost", 8080)
+
+  def housePriceSearch(request: HttpRequest): Future[HttpResponse] =
+    Source.single(request).via(housePriceFlow).runWith(Sink.head)
+
+  def searchByPostcode(postcode: String): Future[String] = {
+    housePriceSearch(RequestBuilding.Get(s"/pricepaid/uk/_search?q=$postcode")).flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[String]
+        case BadRequest => Future.successful("woops")
+        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+          val error = s"FAIL - ${response.status}"
+          Future.failed(new IOException(error))
+        }
+      }
+    }
+  }
+
   val route =
     path("") {
       redirect("/houseprices/wa3%206xf", StatusCodes.Found)

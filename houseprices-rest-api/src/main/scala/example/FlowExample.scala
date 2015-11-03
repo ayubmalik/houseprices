6223e2df71bf0a86816038839f34539100042bd4
Ayub Malik <ayub.malik@dwp.gsi.gov.uk>
2015-11-03 13:24:19 +0000

    Add flow example

---
+++ houseprices-rest-api/src/main/scala/example/FlowExample.scala
@@ -0,0 +1,28 @@
+package example
+
+import scala.concurrent.Await
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Keep
+import akka.stream.scaladsl.RunnableGraph
+import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.Source
+
+object FlowExample extends App {
+  implicit val sys = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+
+  val source = Source(1 to 100)
+  val sink = Sink.fold[Int, Int](0)(_ + _)
+  val graph: RunnableGraph[Future[Int]] = source.toMat(sink)(Keep.right)
+
+  val sum = graph.run()
+
+  println(Await.result(sum, 1 second))
+
+  sys.terminate()
+
+}

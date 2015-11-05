c2e03127a8b2fcc9487c37e5698999570dbce52b
Ayub Malik <ayub.malik@dwp.gsi.gov.uk>
2015-11-05 16:38:04 +0000

    chmod it

--- deploy/kill_all_instances.sh
+++ deploy/kill_all_instances.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# find only running instances
 instance_ids=$(aws ec2 describe-instances --output text --filters Name=instance-state-name,Values=running | grep INSTANCES | cut -f8 | tr '\n' ' ')
 if [[ -z "${instance_ids}" ]]; then
   echo "No running instances to terminate"

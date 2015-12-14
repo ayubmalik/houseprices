9b22efc8422d51171c9ae301b309eb7c6372f8ed
Ayub Malik <ayub.malik@dwp.gsi.gov.uk>
2015-12-14 16:17:09 +0000

    wip

--- houseprices-core/src/main/scala/houseprices/csv/PricePaidDataFetcher.scala
+++ houseprices-core/src/main/scala/houseprices/csv/PricePaidDataFetcher.scala
@@ -6,13 +6,15 @@ import houseprices.postcodes.PostcodeRepo
 import houseprices.PricePaid
 import houseprices.Address
 import houseprices.postcodes.FileSource
+import org.apache.http.client.HttpClient
+import org.apache.http.impl.client.DefaultHttpClient
 
 object PricePaidDataFetcher {
 
   val fullCsvFile = "http://publicdata.landregistry.gov.uk/market-trend-data/price-paid-data/b/pp-complete.csv"
 
   def fetchFullCsv = {
-    val httpClient = new HttpClien
+    val httpClient = new DefaultHttpClient()
   }
 
 }

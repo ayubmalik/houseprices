18abb10d69524ff497f5a2803443bb6600ce5cd9
Ayub Malik <ayub.malik@dwp.gsi.gov.uk>
2016-02-18 14:29:51 +0000

    Adding test for GET /admin/datadownloads

--- houseprices-core/src/test/scala/houseprices/admin/api/AdminServerSpec.scala
+++ houseprices-core/src/test/scala/houseprices/admin/api/AdminServerSpec.scala
@@ -2,15 +2,25 @@ package houseprices.admin.api
 
 import org.scalatest.Matchers
 import org.scalatest.WordSpec
+
+import akka.http.scaladsl.model.HttpMethods
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.Uri.apply
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 
-class AdminServerSpec extends WordSpec with Matchers with ScalatestRouteTest {
+class AdminServerSpec extends WordSpec
+    with Matchers with ScalatestRouteTest with AdminService {
 
   "Admin Server" when {
 
     "url is /admin/datadownloads" should {
 
       "return 200 for GET" in {
+
+        val getRequest = HttpRequest(HttpMethods.GET, uri = "/admin/datadownloads")
+        getRequest ~> routes ~> check {
+          status.isSuccess() shouldEqual true
+        }
 
       }
     }

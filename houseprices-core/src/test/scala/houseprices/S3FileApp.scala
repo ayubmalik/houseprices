1dd9c1d4e430dad899e82ccf90bda702c377f2e1
Ayub Malik <ayub.malik@gmail.com>
2016-02-26 16:48:34 +0000

    spike

--- houseprices-core/src/test/scala/houseprices/S3FileApp.scala
+++ houseprices-core/src/test/scala/houseprices/S3FileApp.scala
@@ -1,6 +1,11 @@
 package houseprices
 
 import scala.io.Source
+import java.time.Period
+import java.time.LocalDate
+import java.time.temporal.TemporalUnit
+import java.time.temporal.ChronoUnit
+import java.time.LocalDateTime
 
 object S3FileApp extends App {
   val url1 = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update-new-version.csv"
@@ -8,11 +13,9 @@ object S3FileApp extends App {
 
   lazy val lines = Source.fromURL(url2).getLines
 
-  var count = 0
-  //  for (row <- lines) {
-  //    println(row)
-  //    count += 1
-  //  }
-  count = lines.size
-  println("\n\nCount: " + count)
+  val start = LocalDateTime.now
+  val count = lines.size
+  val end = LocalDateTime.now
+  val seconds = ChronoUnit.MILLIS.between(start, end) / 1000.0
+  println("\nCount: " + count + " took: " + seconds + "s")
 }

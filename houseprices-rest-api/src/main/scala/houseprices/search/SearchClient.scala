b42eaf440e9be70b01ac51201e631f950ae75b2b
Ayub Malik <ayub.malik@gmail.com>
2016-02-28 17:00:28 +0000

    Lots of testing stuff

--- houseprices-rest-api/src/main/scala/houseprices/search/SearchClient.scala
+++ houseprices-rest-api/src/main/scala/houseprices/search/SearchClient.scala
@@ -1,15 +1,41 @@
 package houseprices.search
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpMethods,HttpRequest, HttpResponse}
+import org.slf4j.LoggerFactory
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
 case class Query(query: String)
 
 trait SearchClient {
   def search(query: Query): SearchResult
 }
 
-class HttpSearchClient extends SearchClient {
-  def search(query: Query) = SearchResult(0)
+trait HttpRequestFactory {
+  def request(req: HttpRequest): Future[HttpResponse]
+}
+
+trait AkkaHttpRequestFactory extends HttpRequestFactory {
+  implicit val system: ActorSystem
+  implicit val materializer = ActorMaterializer()
+  val http = Http(system)
+  def request(req: HttpRequest) = http.singleRequest(req)
+}
+
+class HttpSearchClient(implicit val system: ActorSystem) extends SearchClient {
+  this: HttpRequestFactory =>
+  val log = LoggerFactory.getLogger(getClass)
+  def search(query: Query) = {
+    val response = Await.result(request(HttpRequest(HttpMethods.GET,
+      uri = "http://localhost:9200/pricepaid/uk/_search?q=")), 3.seconds)
+    log.info("response:" + response)
+    SearchResult(0) // TODO: unmarshalling etc
+  }
 }
 
 object HttpSearchClient {
-  def apply() = new HttpSearchClient
+  def apply(implicit system: ActorSystem) = new HttpSearchClient with AkkaHttpRequestFactory
 }
--- houseprices-rest-api/src/test/scala/houseprices/search/SearchClientSpec.scala
+++ houseprices-rest-api/src/test/scala/houseprices/search/SearchClientSpec.scala
@@ -2,16 +2,38 @@ package houseprices.search
 
 import org.scalatest.WordSpec
 import org.scalatest.Matchers
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse, MediaTypes}
+import scala.concurrent.Future
 
-class SearchClientSpeac extends WordSpec with Matchers {
+class SearchClientSpec extends WordSpec with Matchers {
 
-  "SearchClient" should {
+  implicit val system = ActorSystem("test")
+  implicit val ec = system.dispatcher
 
-    "should return search result" in {
-      val searchClient = HttpSearchClient()
-      val result = searchClient.search(Query("hello"))
-      result.count should be(0)
-    }
+  trait StubHttpRequestFactory extends HttpRequestFactory {
+    import MediaTypes.`application/json`
+    val data = """{ "hits": 0 }"""
+    def request(req: HttpRequest) = Future[HttpResponse] {
+      HttpResponse(entity = HttpEntity(`application/json`, data))}
   }
 
+  "SearchClient" when {
+
+    "searching" should {
+
+      "return search result" in {
+        val searchClient = new HttpSearchClient with StubHttpRequestFactory
+        val result = searchClient.search(Query("hello"))
+        result.count should be(0)
+      }
+
+      "return count of two" ignore {
+        val searchClient = new HttpSearchClient with StubHttpRequestFactory
+        val result = searchClient.search(Query("world"))
+        result.count should be(2)
+      }
+
+    }
+  }
 }

e99d95c346dfe35ff77cf175700272fcd424e45a
Ayub Malik <ayub.malik@gmail.com>
2016-03-02 11:36:37 +0000

    Adding blank spec for server

--- houseprices-rest-api/src/test/scala/houseprices/search/HousePriceSearchServerSpec.scala
+++ houseprices-rest-api/src/test/scala/houseprices/search/HousePriceSearchServerSpec.scala
@@ -2,19 +2,29 @@ package houseprices.search
 
 import org.scalatest.Matchers
 import org.scalatest.WordSpec
-
 import akka.actor.ActorSystem
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.model.HttpMethods
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.HttpEntity
 
-class HousePriceSearchServerSpec extends WordSpec with Matchers {
+class HousePriceSearchServerSpec extends WordSpec
+    with Matchers with ScalatestRouteTest with HousePriceSearchService {
 
-  implicit val system = ActorSystem("test")
   implicit val ec = system.dispatcher
+
+  val searchClient = null
 
   "HousePriceSearchServer" when {
 
     "searching" should {
 
       "return search result" in {
+        val getRequest = HttpRequest(HttpMethods.GET, uri = "/search/meh")
+        getRequest ~> routes ~> check {
+          status.isSuccess() shouldEqual true
+          //responseEntity shouldEqual HttpEntity(`application/json`, active.toJson.prettyPrint)
+        }
 
       }
 
